22fa2bfa863c1e176b91e776f5572a6569d01bb8
curioushoainam <hoainamtruongute@gmail.com>
2019-03-03 17:33:48 +0700

    Javascript Objects

--- js_demo.js
+++ js_demo.js
@@ -33,14 +33,54 @@
 //      string
 //      (symbol)
 // Objects
-const x = 42;
-const explicit = String(x);         //  explicit === "42"
-const implict = x + "";             //  implicit === "42"
+// const x = 42;
+// const explicit = String(x);         //  explicit === "42"
+// const implict = x + "";             //  implicit === "42"
 
-console.log(typeof x);              // number
+// console.log(typeof x);              // number
 
-console.log(typeof null);           // object
+// console.log(typeof null);           // object
 
 // https://dorey.github.io/JavaScript-Equality-Table/
+
+//-------------------------------
+// Javascript Object
+const o = new Object();
+o.firstName = "Nam"
+o.lastName = "Truong"
+o.isWorking = true
+o.greet = function(){
+    console.log('hi!')
+}
+
+const o2 = {}
+o2.firstName = "Nam"
+o2['lastName'] = "Truong"
+const key = "isWorking"
+o2[key] = true
+o2['greet'] = function(){
+    console.log('hi!')
+}
+
+const o3 = {
+    1: 'test',
+    firstName : "Nam",
+    lastName : "Truong",
+    isWorking : true,
+    greet : function(){
+        console.log('hi!')
+    },
+    address: {
+        street: "number 5",
+        number: "36/2",
+    },
+}
+
+// console.log(o)
+// console.log(o2)
+// console.log(o3)
+// console.log(o3.1)       // error
+// console.log(o3[1])          //  = console.log(o3["1"])
+
 
 
