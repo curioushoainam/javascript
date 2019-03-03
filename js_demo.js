4b055bb0a5cb67c528b35a79bb8afe054ee908d1
curioushoainam <hoainamtruongute@gmail.com>
2019-03-03 18:09:56 +0700

    Object mutating

--- js_demo.js
+++ js_demo.js
@@ -45,42 +45,87 @@
 
 //-------------------------------
 // Javascript Object
-const o = new Object();
-o.firstName = "Nam"
-o.lastName = "Truong"
-o.isWorking = true
-o.greet = function(){
-    console.log('hi!')
-}
+// const o = new Object();
+// o.firstName = "Nam"
+// o.lastName = "Truong"
+// o.isWorking = true
+// o.greet = function(){
+//     console.log('hi!')
+// }
 
-const o2 = {}
-o2.firstName = "Nam"
-o2['lastName'] = "Truong"
-const key = "isWorking"
-o2[key] = true
-o2['greet'] = function(){
-    console.log('hi!')
-}
+// const o2 = {}
+// o2.firstName = "Nam"
+// o2['lastName'] = "Truong"
+// const key = "isWorking"
+// o2[key] = true
+// o2['greet'] = function(){
+//     console.log('hi!')
+// }
 
-const o3 = {
-    1: 'test',
-    firstName : "Nam",
-    lastName : "Truong",
-    isWorking : true,
-    greet : function(){
-        console.log('hi!')
-    },
-    address: {
-        street: "number 5",
-        number: "36/2",
-    },
-}
+// const o3 = {
+//     1: 'test',
+//     firstName : "Nam",
+//     lastName : "Truong",
+//     isWorking : true,
+//     greet : function(){
+//         console.log('hi!')
+//     },
+//     address: {
+//         street: "number 5",
+//         number: "36/2",
+//     },
+// }
 
 // console.log(o)
 // console.log(o2)
 // console.log(o3)
 // console.log(o3.1)       // error
 // console.log(o3[1])          //  = console.log(o3["1"])
+
+//-------------------------------
+// Objects Mutation
+const o = {
+    a: 'a',
+    b: 'b',
+    obj: {
+        key: 'key',
+    }
+}
+
+// const o2 = o
+// o.a = 'new value'
+// console.log(o2.a)       // new value
+
+const o2 = Object.assign({}, o)
+// o.a = 'new value'
+// console.log(o2.a)            // a
+// o.obj.key = 'new value'
+// console.log(o2.obj.key)         // new value
+
+//  deep copy
+function deepCopy(obj){
+    // check if vals are objects
+    // if so. copy that object (deep copy)
+    // else return the value
+    const keys = Object.keys(obj)
+
+    const newObject = {}
+
+    for (let i = 0; i < keys.length; i++){
+        const key = keys[i]
+        if(typeof obj[key] === 'object') {
+            newObject[key] = deepCopy(obj[key])
+        } else {
+            newObject[key] = obj[key]
+        }
+    }
+
+    return newObject
+}
+
+const o3 = deepCopy(o)
+o.obj.key = 'new value!'
+console.log(o3.obj.key)         // key
 
 
 
