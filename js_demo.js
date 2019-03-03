3e35ef7621e3424258c78d5582642723841b8f34
curioushoainam <hoainamtruongute@gmail.com>
2019-03-03 17:19:04 +0700

    Javascript Types

--- js_demo.js
+++ js_demo.js
@@ -3,23 +3,44 @@
 //-------------------------------
 // Javascript Syntax
 
-const firstName = "Nam"
-const lastName = "Truong"
+// const firstName = "Nam"
+// const lastName = "Truong"
 
-const val = 42
+// const val = 42
 
-const arr = [
-    'string',
-    42,
-    function(){
-        console.log('hi')
-    }
-]
+// const arr = [
+//     'string',
+//     42,
+//     function(){
+//         console.log('hi')
+//     }
+// ]
 
-arr[2]()
+// arr[2]()
 
-for (let i=0; i<arr.length; i++){
-    console.log(arr[i])
-}
+// for (let i=0; i<arr.length; i++){
+//     console.log(arr[i])
+// }
 
 //-------------------------------
+// Javascript Types
+// Dynamic typing
+// Primitive types (no methods, immutable)
+//      undefined
+//      null
+//      boolean
+//      number
+//      string
+//      (symbol)
+// Objects
+const x = 42;
+const explicit = String(x);         //  explicit === "42"
+const implict = x + "";             //  implicit === "42"
+
+console.log(typeof x);              // number
+
+console.log(typeof null);           // object
+
+// https://dorey.github.io/JavaScript-Equality-Table/
+
+
